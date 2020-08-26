60cf84f4964446210655324894332977bcc1c2ce
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2020-08-25 22:08:05 -0700

    Solution with slice, but case sensitive

--- 2020-08-25/word_counter.py
+++ 2020-08-25/word_counter.py
@@ -6,19 +6,23 @@ def main():
 
     punctiotionSet = ('.', ',', ':', '"', '!', '?', ' ', '(', ')')
 
-    word = ""
-    for c in input:
+    start = 0
+    length = 0
+    for i, c in enumerate(input):
         c = c.lower()
         if c in punctiotionSet:
-            if not word:
+            if not length:
+                start = i + 1
                 continue
+            word = input[start: start + length]
             if word in word_map.keys():
                 word_map[word] += 1
             else:
                 word_map[word] = 1
-            word = ""
+            start = i + 1
+            length = 0
         else:
-            word += c
+            length += 1
 
     print(sorted(word_map.keys()))
     print(word_map)
